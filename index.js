a00cc29568a405653a6916d47691e32ff1a0cc20
sbalayan1 <balayans2014@yahoo.com>
2022-05-03 07:05:39 -0700

    commit tree 3c44d753863e97e2b060c9537c8ebb1251e018da

--- index.js
+++ index.js
@@ -1,51 +1,39 @@
 // Write your solution here!
 const cats = ['Milo', 'Otis', 'Garfield'];
 
-
 function destructivelyAppendCat(name) {
-    cats.push(name);
+    // array => invoking the push methond on the array => passing the name as an argument
+    cats.push(name)
 }
-
-destructivelyAppendCat('Ralph');
 
 function destructivelyPrependCat(name) {
     cats.unshift(name);
 }
 
-destructivelyPrependCat('Arthur');
 
 function destructivelyRemoveLastCat() {
     cats.pop();
 }
 
-destructivelyRemoveLastCat();
 
 function destructivelyRemoveFirstCat() {
     cats.shift();
 }
-
-destructivelyRemoveFirstCat();
 
 function appendCat(name) {
     const newCats = [...cats,name];
     return newCats;
 } 
 
-appendCat('Dog');
-
-
 function prependCat(name) {
     const oldCats = [name,...cats];
     return oldCats;
 }
 
-prependCat('Onyx');
-
 function removeLastCat() {
    const deadNewCats = cats.slice(0,-1);
    return deadNewCats;
 }
-
 
 function removeFirstCat() {
     const deadOldCats = cats.slice(1);
